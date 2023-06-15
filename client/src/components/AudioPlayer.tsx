c670d80838d6fba3d1745df51ab59d2891e3f8a4
unknown <samuelowusu958@gmail.com>
2023-06-15 08:47:26 +0000

    first commit

--- client/src/components/AudioPlayer.tsx
+++ client/src/components/AudioPlayer.tsx
@@ -1,19 +1,5 @@
-import { useEffect } from "react";
-import WaveSurfer from "wavesurfer.js";
-
 function AudioPlayer() {
-  useEffect(() => {
-    const wavesurfer = WaveSurfer.create({
-      container: "#waveform",
-      waveColor: "violet",
-      progressColor: "purple",
-      cursorColor: "navy",
-    });
-
-    wavesurfer.load("audio.mp3");
-  }, []);
-
-  return <div id="waveform" className="border border-red-500"></div>;
+  return <div>AudioPlayer</div>;
 }
 
 export default AudioPlayer;
